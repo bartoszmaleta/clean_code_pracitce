ddd2f0ec6ffa13d8820451af3911d48f3317a6d3
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-11-21 22:33:39 +0100

    Changed everything to funcs and add parameters

--- code_refactored.py
+++ code_refactored.py
@@ -20,33 +20,42 @@ def return_second_list_with_10_random_numbers():
     return b
 
 
+def geussing_from_a_list(list_with_random_numbers):
+    for i in range(amount_of_numbers_in_a_list):
+        g = int(input("Enter an integer from 1 to 99: "))
+        while list_with_random_numbers[i] != g:
+            if g < list_with_random_numbers[i]:
+                print("guess is low")
+                g = int(input("Enter an integer from 1 to 99: "))
+            elif g > list_with_random_numbers[i]:
+                print("guess is high")
+                g = int(input("Enter an integer from 1 to 99: "))
+            else:
+                break
+        win_text = "you guessed it!"
+        print(win_text)
+        # return win_text
+
+
+def geussing_from_b_list(list_with_random_numbers):
+    for i in range(amount_of_numbers_in_a_list):
+        g = int(input("Enter an integer from 1 to 49: "))
+        while list_with_random_numbers[i] != g:
+            if g < list_with_random_numbers[i]:
+                print("guess is low")
+                g = int(input("Enter an integer from 1 to 49: "))
+            elif g > list_with_random_numbers[i]:
+                print("guess is high")
+                g = int(input("Enter an integer from 1 to 49: "))
+            else:
+                break
+        win_text = "you guessed it!"
+        print(win_text)
+        # return win_text
+
+
 list_with_10_numbers = return_list_with_10_random_numbers()
-
-for i in range(amount_of_numbers_in_a_list):
-    g = int(input("Enter an integer from 1 to 99: "))
-    while list_with_10_numbers[i] != g:
-        if g < list_with_10_numbers[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 99: "))
-        elif g > list_with_10_numbers[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 99: "))
-        else:
-            break
-    print("you guessed it!")
-
+geussing_from_a_list(list_with_10_numbers)
 
 second_list_with_10_numbers = return_second_list_with_10_random_numbers()
-
-for i in range(amount_of_numbers_in_a_list):
-    g = int(input("Enter an integer from 1 to 49: "))
-    while second_list_with_10_numbers[i] != g:
-        if g < second_list_with_10_numbers[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 49: "))
-        elif g > second_list_with_10_numbers[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+geussing_from_b_list(second_list_with_10_numbers)
