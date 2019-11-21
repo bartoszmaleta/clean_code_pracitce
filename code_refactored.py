eb05f0efa414aaa6b9e1c370a711a980a5cfd0fe
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-11-21 22:36:13 +0100

    No global variables

--- code_refactored.py
+++ code_refactored.py
@@ -1,26 +1,28 @@
-import math
 import random
-
-a = []
-b = []
-amount_of_numbers_in_a_list = 10
-lower_bound_of_range = 1
-high_bound_of_range = 99
 
 
 def return_list_with_10_random_numbers():
+    amount_of_numbers_in_a_list = 10
+    lower_bound_of_range = 1
+    high_bound_of_range = 99
+    a = []
     for i in range(amount_of_numbers_in_a_list):
         a.append(random.randint(lower_bound_of_range, high_bound_of_range))
     return a
 
 
 def return_second_list_with_10_random_numbers():
+    amount_of_numbers_in_a_list = 10
+    lower_bound_of_range = 1
+    high_bound_of_range = 99
+    b = []
     for i in range(amount_of_numbers_in_a_list):
         b.append(random.randint(lower_bound_of_range, high_bound_of_range))
     return b
 
 
 def geussing_from_a_list(list_with_random_numbers):
+    amount_of_numbers_in_a_list = 10
     for i in range(amount_of_numbers_in_a_list):
         g = int(input("Enter an integer from 1 to 99: "))
         while list_with_random_numbers[i] != g:
@@ -34,10 +36,10 @@ def geussing_from_a_list(list_with_random_numbers):
                 break
         win_text = "you guessed it!"
         print(win_text)
-        # return win_text
 
 
 def geussing_from_b_list(list_with_random_numbers):
+    amount_of_numbers_in_a_list = 10
     for i in range(amount_of_numbers_in_a_list):
         g = int(input("Enter an integer from 1 to 49: "))
         while list_with_random_numbers[i] != g:
@@ -51,7 +53,6 @@ def geussing_from_b_list(list_with_random_numbers):
                 break
         win_text = "you guessed it!"
         print(win_text)
-        # return win_text
 
 
 list_with_10_numbers = return_list_with_10_random_numbers()
