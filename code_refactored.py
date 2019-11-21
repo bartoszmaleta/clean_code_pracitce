4fc710c9db47b1dc701e714fa15b09b026541133
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-11-21 22:19:03 +0100

    Changed magic numbers and changed funcs to create lists

--- code_refactored.py
+++ code_refactored.py
@@ -2,30 +2,30 @@ import math
 import random
 
 a = []
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
-# a.append(random.randint(1, 99))
+b = []
+amount_of_numbers_in_a_list = 10
+lower_bound_of_range = 1
+high_bound_of_range = 99
 
 
 def return_list_with_10_random_numbers():
-    for i in range(10):
-        a.append(random.randint(1, 99))
+    for i in range(amount_of_numbers_in_a_list):
+        a.append(random.randint(lower_bound_of_range, high_bound_of_range))
     return a
+
+
+def return_second_list_with_10_random_numbers():
+    for i in range(amount_of_numbers_in_a_list):
+        b.append(random.randint(lower_bound_of_range, high_bound_of_range))
+    return b
 
 
 list_with_10_numbers = return_list_with_10_random_numbers()
 
-for i in range(1):
+for i in range(amount_of_numbers_in_a_list):
     g = int(input("Enter an integer from 1 to 99: "))
     while list_with_10_numbers[i] != g:
-        if g < a[i]:
+        if g < list_with_10_numbers[i]:
             print("guess is low")
             g = int(input("Enter an integer from 1 to 99: "))
         elif g > list_with_10_numbers[i]:
@@ -35,24 +35,16 @@ for i in range(1):
             break
     print("you guessed it!")
 
-b = []
-b.append(random.randint(1, 49)) 
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-for i in range(10):
+
+second_list_with_10_numbers = return_second_list_with_10_random_numbers()
+
+for i in range(amount_of_numbers_in_a_list):
     g = int(input("Enter an integer from 1 to 49: "))
-    while b[i] != g:
-        if g < b[i]:
+    while second_list_with_10_numbers[i] != g:
+        if g < second_list_with_10_numbers[i]:
             print("guess is low")
             g = int(input("Enter an integer from 1 to 49: "))
-        elif g > b[i]:
+        elif g > second_list_with_10_numbers[i]:
             print("guess is high")
             g = int(input("Enter an integer from 1 to 49: "))
         else:
